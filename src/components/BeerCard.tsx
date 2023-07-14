1593dc67f13d752d8ab9f90195faf9fa7810e86a
Yumi-Namie <rn.yumi@gmail.com>
2023-07-14 22:51:03 +0200

    card perfect

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -20,15 +20,15 @@ export const BeerCard = ({ beer }: BeerCardProps) => {
   return (
     <Link className="beer-card" to={`/beers/${beer._id}`}>
       <Card className="w-100" key={beer._id}>
-        <Row className="beer-card-row d-flex justify-content-start align-items-center">
-          <Col xs={4} >
-            <Card.Img variant="top" src={beer.image_url} style={{ width: "auto", height: "9rem", objectFit: "cover" }} />
+        <Row className="beer-card-row d-flex justify-content-center align-items-center">
+          <Col xs={3} className="p-0" >
+            <Card.Img src={beer.image_url} style={{ width: "auto", height: "6rem", objectFit: "cover" }} />
           </Col>
-          <Col xs={7} className=" d-flex ml-auto m-0">
+          <Col xs={8} className=" d-flex ml-auto p-0">
             <Card.Body className="text-start p-0 w-100">
               <Card.Title style={{ fontSize: "1.2rem" }} >{beer.name}</Card.Title>
               <Card.Text className="text-secondary mb-1" style={{ fontSize: ".9rem" }}>{beer.tagline}</Card.Text>
-              <Card.Text className="mt-1" style={{ fontSize: ".6rem" }}> Create by: {beer.name}</Card.Text>
+              <Card.Text className="mt-1" style={{ fontSize: ".6rem" }}> <strong>Create by:</strong> {beer.name}</Card.Text>
             </Card.Body>
           </Col>
         </Row>
